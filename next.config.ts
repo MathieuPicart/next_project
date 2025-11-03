1bbe72fb0dcd2e1502a79aa3578e1a473086a21f
Mathieu <mat.pic@outlook.fr>
2025-11-03 12:44:48 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+      ignoreBuildErrors: true,
+  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
